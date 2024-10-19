8c864757406b0fb5f2f09c946f3f636ab1c2a595
Rick Jonkman <rickjonkman@MacBook-Air-van-Rick.local>
2024-10-19 10:10:33 +0900

    first commit

---
+++ src/main/java/dev/rick/expensetracker02/ExpenseTracker02Application.java
@@ -0,0 +1,13 @@
+package dev.rick.expensetracker02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTracker02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseTracker02Application.class, args);
+    }
+
+}
